de3a9057ac26a8d79cce354cc885d1f16edcbd4d
ancyrweb <ancyr@pm.me>
2025-03-23 05:07:46 +0100

    task: isolating

--- tests/config/tested-drivers.ts
+++ tests/config/tested-drivers.ts
@@ -1,4 +1,13 @@
 import { InMemoryTester } from './in-memory-tester.js';
 import { Tester } from './tester.js';
 
-export const getTestedDrivers = (): Tester[] => [new InMemoryTester()];
+export const getTestedDrivers = (props?: { isolate?: any }): Tester[] => {
+  const drivers = [new InMemoryTester()];
+
+  if (props?.isolate) {
+    const klass = props.isolate;
+    return drivers.filter((driver) => driver instanceof klass);
+  }
+
+  return drivers;
+};
